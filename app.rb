ac003676df8e41ddd310dc148f2c5152857fe61a
Erik Prenzlin <eprenzlin@gmail.com>
2020-05-22 17:14:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,12 +15,12 @@ class App < Sinatra::Base
 
   get '/goodbye/:name' do
   @user_name = params[:name]
-  "Goodbye, #{@user_name}"
+  "Goodbye,#{@user_name}"
   end
 
   get '/multiply/:number1/:number2' do
   @total.to_s = (params[:number1.to_i] * params[:number2.to_i])
   "#{@total}"
   end
-  
+
 end
